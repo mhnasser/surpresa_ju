4f3bfe689359e6dd5436ea22da2d5f7a909b5a69
mhnasser <mohamad.nasser@outlook.com.br>
2022-05-24 14:05:33 -0300

    deploy update

--- application.py
+++ application.py
@@ -3,37 +3,44 @@ import os
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def pag_inicial():
 
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @application.route("/playlists")
 def pag_playlist():
 
-    return render_template('album.html')
+    return render_template("album.html")
+
 
 @application.route("/Pisero")
 def musicas_piseiro():
 
-    return render_template('pisero.html')
+    return render_template("pisero.html")
+
 
 @application.route("/Sertanejo")
 def musicas_sertanejo():
 
-    return render_template('sertanejo.html')
+    return render_template("sertanejo.html")
+
 
 @application.route("/Lovesongs")
 def musicas_lovesongs():
 
-    return render_template('lovesongs.html')
+    return render_template("lovesongs.html")
+
 
 @application.route("/Us")
 def musicas_us():
 
-    return render_template('us.html')
+    return render_template("us.html")
 
 
 if __name__ == "__main__":
 
-    application.run(host='0.0.0.0', port = 80)
+    application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
